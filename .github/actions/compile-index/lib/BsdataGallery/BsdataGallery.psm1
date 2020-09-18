65d49fcdf9b34ebce0495f9bc31484b04d075ff2
Anthony <anthonyjohnsonadams@gmail.com>
2020-09-18 20:09:54 +0100

    Update last updated timestamps

--- .github/actions/compile-index/lib/BsdataGallery/BsdataGallery.psm1
+++ .github/actions/compile-index/lib/BsdataGallery/BsdataGallery.psm1
@@ -19,9 +19,10 @@ function Get-BsdataGalleryCatpkg {
     battleScribeVersion = ($entries.cache.catpkg.properties.battleScribeVersion | Sort-Object -Bottom 1) -as [string]
   } + $GallerySettings.urls + @{
     repositories = @($entries.cache | ForEach-Object {
-        $_.catpkg.properties.archived = $_.repo.archived -eq $true
-        return $_.catpkg.properties
-      })
+      $_.catpkg.properties.archived = $_.repo.archived -eq $true
+      $_.catpkg.properties.lastUpdated = [datetime]::ParseExact($_.catpkg.properties.lastUpdated, 'MM/dd/yyyy HH:mm:ss', $null).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff+0000")
+      return $_.catpkg.properties
+    })
   }
   return $galleryJsonContent
 }
@@ -67,7 +68,7 @@ function Get-GHApiUpdatedResult {
     [Parameter()]
     [int]$RetryIntervalSec = 5
   )
-  
+
   # get object from API, but only if newer than what we've got already
   try {
     $apiArgs = @{
@@ -260,11 +261,11 @@ function Update-BsdataGalleryIndex {
     # Path to index entries directory
     [Parameter(Mandatory)]
     [string]$IndexPath,
-    
+
     [Parameter(Mandatory)]
     [string]$Token
   )
-  
+
   # read registry entries
   $registry = [ordered]@{ }
   Get-ChildItem $RegistrationsPath *.catpkg.yml | Sort-Object Name | ForEach-Object {
