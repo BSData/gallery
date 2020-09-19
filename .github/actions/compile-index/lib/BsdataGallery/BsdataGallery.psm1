29b17d0aebba7d02f990c55bde127292a4ff5c07
Anthony <anthonyjohnsonadams@gmail.com>
2020-09-19 19:53:54 +0100

    Please our boy amis

--- .github/actions/compile-index/lib/BsdataGallery/BsdataGallery.psm1
+++ .github/actions/compile-index/lib/BsdataGallery/BsdataGallery.psm1
@@ -20,7 +20,7 @@ function Get-BsdataGalleryCatpkg {
   } + $GallerySettings.urls + @{
     repositories = @($entries.cache | ForEach-Object {
       $_.catpkg.properties.archived = $_.repo.archived -eq $true
-      $_.catpkg.properties.lastUpdated = [datetime]::ParseExact($_.catpkg.properties.lastUpdated, 'MM/dd/yyyy HH:mm:ss', $null).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff+0000")
+      $_.catpkg.properties.lastUpdated = $_.catpkg.properties.lastUpdated.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff+0000")
       return $_.catpkg.properties
     })
   }
