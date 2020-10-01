2471cf1ae0171111c3fa01b5238f70d7d955b4c7
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-10-01 15:44:03 +0200

    ci: Fix CI script to be locally runnable

--- .github/actions/validate-registry/action.ps1
+++ .github/actions/validate-registry/action.ps1
@@ -4,45 +4,47 @@ param (
   [string[]] $Entries,
 
   [Parameter()]
+  [string] $RegistryPath = (Get-Item "./registry"),
+
+  [Parameter()]
   [string] $Token
 )
-$Entries = $Entries -replace '\', '/'
+
+$Entries = $Entries -replace '\\', '/'
 Import-Module powershell-yaml -Verbose:$false -ErrorAction:Ignore
 || Install-Module powershell-yaml -RequiredVersion 0.4.2 -Force -Verbose:$false
 
 if ($env:GITHUB_ACTIONS -ne 'true') {
-  function Send-ActionCommand {
-    [CmdletBinding()]
-    param (
-      [Parameter(Mandatory)]
-      [string] $Command,
-
-      [Parameter(Mandatory)]
-      [System.Collections.IDictionary] $Properties,
-
-      [Parameter()]
-      [string] $Message
-    )
-    function FormatFileRef($props, $msg) {
-      $file = $props['file']
-      $line = $props['line']
-      $col = $props['col']
-      $value = ''
-      if ($file) {
-        $position = $line ? $col ? "($line,$col)" : "($line)" : '';
-        $value += "${file}${position}: "
-      }
-      return $value + $message
+  function LogDebug($Message) {
+    Write-Verbose $Message
+  }
+  function LogWarning($Message, $File) {
+    Write-Warning "$File`: $Message"
+  }
+  function LogError($Message, $File) {
+    Write-Error "$File`: $Message"
+  }
+  function FormatFileRef($props, $msg) {
+    $file = $props['file']
+    $line = $props['line']
+    $col = $props['col']
+    $value = ''
+    if ($file) {
+      $position = $line ? $col ? "($line,$col)" : "($line)" : '';
+      $value += "${file}${position}: "
     }
-    switch ($Command) {
-      'error' { Write-Error (FormatFileRef $Properties $Message) }
-      'warning' { Write-Warning (FormatFileRef $Properties $Message) }
-      'debug' { Write-Verbose $Message }
-      Default { 
-        $cmdStr = ConvertTo-ActionCommandString $Command $Properties $Message
-        Write-Host $cmdStr
-      }
-    }
+    return $value + $message
+  }
+}
+else {
+  function LogDebug($Message) {
+    Write-ActionDebug $Message
+  }
+  function LogWarning($Message, $File) {
+    Write-ActionWarning $Message $File
+  }
+  function LogError($Message, $File) {
+    Write-ActionError $Message $File
   }
 }
 
@@ -53,17 +55,17 @@ $entriesDir = Join-Path $RegistryPath $settings.registrations.path
 # process registry entries
 Get-ChildItem $entriesDir *.catpkg.yml | Sort-Object Name | ForEach-Object {
   if ($Entries -and [System.IO.Path]::GetRelativePath('.', "$_").Replace('\', '/') -notin $Entries) {
-    Write-ActionDebug "$_ skipped because it wasn't whitelisted to check."
+    LogDebug "$_ skipped because it wasn't whitelisted to check."
   }
   $file = $_
   $entry = Get-Content $file -Raw | ConvertFrom-Yaml -Ordered
   $repo = $entry.location.github
   if (-not $repo) {
-    Write-ActionError "Entry must have a location.github property." $file
+    LogError "Entry must have a location.github property." $file
     return
   }
   if ($repo -notmatch '^[^\s\/]+\/[^\s\/]+$') {
-    Write-ActionError "GitHub repository name must be formatted as 'owner/name', e.g. BSData/gallery." $file
+    LogError "GitHub repository name must be formatted as 'owner/name', e.g. BSData/gallery." $file
     return
   }
   $owner, $reponame = $repo -split '/'
@@ -80,14 +82,14 @@ Get-ChildItem $entriesDir *.catpkg.yml | Sort-Object Name | ForEach-Object {
   $apiRepo = Invoke-RestMethod @apiRepoArgs
   if ($status -ne 200) {
     if ($status -eq 404) {
-      Write-ActionError "GitHub repository couldn't be reached at $apiUrl - it doesn't exist or is private." $file
+      LogError "GitHub repository couldn't be reached at $apiUrl - it doesn't exist or is private." $file
       return
     }
-    Write-ActionError "Fetching $repo failed with HTTP $status." $file
+    LogError "Fetching $repo failed with HTTP $status." $file
     return
   }
   if ('battlescribe-data' -notin $apiRepo.topics) {
-    Write-ActionWarning "$repo doesn't have a 'battlescribe-data' topic added." $file
+    LogWarning "$repo doesn't have a 'battlescribe-data' topic added." $file
   }
   $getWorkflowsArgs = @{
     Uri = "$apiUrl/contents/.github/workflows"
@@ -98,10 +100,10 @@ Get-ChildItem $entriesDir *.catpkg.yml | Sort-Object Name | ForEach-Object {
   $workflows = Invoke-RestMethod @getWorkflowsArgs
   $addWorkflowsSuggestion = @"
 Consider adding necessary workflows by adding a comment with first line like:
-> `/template-workflows-pr $repo`
+> ``/template-workflows-pr $repo``
 "@
   if ($status -ne 200) {
-    Write-ActionWarning "Fetching $repo workflows failed with HTTP $status. $addWorkflowsSuggestion" $file
+    LogWarning "Fetching $repo workflows failed with HTTP $status. $addWorkflowsSuggestion" $file
   }
   $existingWorkflows = @(($workflows | Where-Object { $_.type -eq 'file' }).name)
   $missingWorkflows = @(
@@ -110,6 +112,6 @@ Consider adding necessary workflows by adding a comment with first line like:
     'publish-catpkg.yml'
   ) | Where-Object { $_ -notin $existingWorkflows }
   if ($missingWorkflows) {
-    Write-ActionWarning "$repo is missing workflows: $($missingWorkflows -join ', '). $addWorkflowsSuggestion" $file
+    LogWarning "$repo is missing workflows: $($missingWorkflows -join ', '). $addWorkflowsSuggestion" $file
   }
 }
