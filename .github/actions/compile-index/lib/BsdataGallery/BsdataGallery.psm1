74a085da5dcca5d75835f092f6a7ae8802ed578f
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-10-12 12:30:55 +0200

    handle API rate limit by throwing in Compile Index

--- .github/actions/compile-index/lib/BsdataGallery/BsdataGallery.psm1
+++ .github/actions/compile-index/lib/BsdataGallery/BsdataGallery.psm1
@@ -95,6 +95,12 @@ function Get-GHApiUpdatedResult {
         $apiResult | Out-Null # to avoid PSUseDeclaredVarsMoreThanAssignments, Justification: scriptblock is executed in current scope
       }
       Write-Verbose ("Request finished in {0:c}" -f $time)
+      # check api rate limit
+      if ($httpStatus -eq [System.Net.HttpStatusCode]::Forbidden -and $respHeaders['X-RateLimit-Remaining'] -eq '0') {
+        # we've hit rate limit
+        $resetTime = [DateTime]::UnixEpoch.AddSeconds($respHeaders['X-Rate-Limit-Reset'])
+        throw "GET $Endpoint blocked by RateLimit. Limit resets at $resetTime in $($resetTime - [datetime]::UtcNow)"
+      }
       # repeat until 3rd attempt or success (200 or 304 is success)
       $repeat = ++$attempts -lt $MaximumRetryCount -and $httpStatus -notin @(200, 304)
       if ($repeat) {
